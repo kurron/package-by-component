addacf335a3a842ed447defd1a47465e085635d8
Ron Kurr <kurr@kurron.org>
2022-10-29 18:43:48 -0400

    Added Kotlin skeleton modules for some components.

---
+++ event-processor/src/test/kotlin/org/kurron/gurps/processor/command/CommandProcessorApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.kurron.gurps.processor.command
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommandProcessorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
