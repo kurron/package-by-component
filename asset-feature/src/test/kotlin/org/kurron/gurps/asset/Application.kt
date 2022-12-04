ec1b711fc779a614ad32b55fde6f34f18629b866
Ron Kurr <kurr@kurron.org>
2022-12-04 11:12:22 -0500

    We only need this for testing. Production only cares about the library.

--- asset-feature/src/test/kotlin/org/kurron/gurps/asset/Application.kt
+++ asset-feature/src/test/kotlin/org/kurron/gurps/asset/Application.kt
@@ -2,6 +2,7 @@ package org.kurron.gurps.asset
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+// We need to have this in order to run the in-memory integration tests. In production, the application will provide the SpringBootApplication.
 @SpringBootApplication
 class Application
 
