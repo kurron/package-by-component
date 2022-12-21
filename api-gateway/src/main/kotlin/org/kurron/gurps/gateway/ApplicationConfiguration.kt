6c53ae84535e998f071fa4f957e981a6da9a2e73
Ron Kurr <kurr@kurron.org>
2022-12-21 18:21:44 -0500

    API Gateway is working, including setting the X-Forward headers and rewriting the Host header to use a stable name.

---
+++ api-gateway/src/main/kotlin/org/kurron/gurps/gateway/ApplicationConfiguration.kt
@@ -0,0 +1,7 @@
+package org.kurron.gurps.gateway
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ApplicationConfiguration {
+}
--- user-feature/src/main/kotlin/org/kurron/gurps/user/UserConfiguration.kt
+++ user-feature/src/main/kotlin/org/kurron/gurps/user/UserConfiguration.kt
@@ -14,6 +14,8 @@ import org.springframework.amqp.rabbit.core.RabbitOperations
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.filter.ForwardedHeaderFilter
+
 
 @Configuration
 class UserConfiguration {
@@ -41,4 +43,10 @@ class UserConfiguration {
         container.setBatchSize(1) // probably want commands to come in one at a time, to help preserve some semblance of ordering
         return container
     }
+
+    @Bean
+    fun forwardedHeaderFilter(): ForwardedHeaderFilter {
+        return ForwardedHeaderFilter()
+    }
+
 }
