fa4457c5f2ecc14f69d6efb1974584aee3c26608
Ron Kurr <kurr@kurron.org>
2022-12-22 16:48:23 -0500

    Require basic authentication to execute any API calls.

--- gurps-online/src/main/kotlin/org/kurron/gurps/ApplicationConfiguration.kt
+++ gurps-online/src/main/kotlin/org/kurron/gurps/ApplicationConfiguration.kt
@@ -2,12 +2,22 @@ package org.kurron.gurps
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.filter.ForwardedHeaderFilter
+
 
 @Configuration
 class ApplicationConfiguration {
     @Bean
     fun forwardedHeaderFilter(): ForwardedHeaderFilter {
         return ForwardedHeaderFilter()
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests{it.anyRequest().authenticated()}.csrf().disable().httpBasic()
+        return http.build()
     }
 }
