192bb9fd36359f6701802c264c5faaa668b12642
rkurr <rkurr@jvmguy.com>
2024-06-26 22:06:49 +0000

    Installed a GURPS home page.

--- campaign-feature/src/main/kotlin/org/kurron/gurps/campaign/CampaignWebPage.kt
+++ campaign-feature/src/main/kotlin/org/kurron/gurps/campaign/CampaignWebPage.kt
@@ -5,7 +5,6 @@ import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
 
 @Controller
 class CampaignWebPage() {
---
+++ gurps-online/src/main/kotlin/org/kurron/gurps/GurpsWebPage.kt
@@ -0,0 +1,23 @@
+package org.kurron.gurps
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@Controller
+class GurpsWebPage() {
+
+    // POST /asset - create
+    // PUT /asset/{id} - update
+    // GET /asset/{id} - read
+    // DELETE /asset/{id} - delete
+    // bulk upload?
+    @GetMapping(path = ["/"], produces = [MediaType.TEXT_PLAIN_VALUE])
+    @ResponseStatus(HttpStatus.OK)
+    fun handleInitialize(): String {
+        val viewName = "gurps-home"
+        return viewName
+    }
+}
