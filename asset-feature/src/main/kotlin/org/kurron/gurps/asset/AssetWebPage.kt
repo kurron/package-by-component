f475625328867b00b9e7dec71f9ac4ac0ef6ea0f
rkurr <rkurr@jvmguy.com>
2024-06-26 21:52:33 +0000

    Fleshing out the web controllers

---
+++ asset-feature/src/main/kotlin/org/kurron/gurps/asset/AssetWebPage.kt
@@ -0,0 +1,24 @@
+package org.kurron.gurps.asset
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@Controller
+class AssetWebPage() {
+
+    // POST /asset - create
+    // PUT /asset/{id} - update
+    // GET /asset/{id} - read
+    // DELETE /asset/{id} - delete
+    // bulk upload?
+    @GetMapping(path = ["/asset-ui"], produces = [MediaType.TEXT_PLAIN_VALUE])
+    @ResponseStatus(HttpStatus.OK)
+    fun handleInitialize(): String {
+        val viewName = "asset-home"
+        return viewName
+    }
+}
---
+++ asset-feature/src/main/kotlin/org/kurron/gurps/campaign/CampaignWebPage.kt
@@ -0,0 +1,24 @@
+package org.kurron.gurps.campaign
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@Controller
+class CampaignWebPage() {
+
+    // POST /asset - create
+    // PUT /asset/{id} - update
+    // GET /asset/{id} - read
+    // DELETE /asset/{id} - delete
+    // bulk upload?
+    @GetMapping(path = ["/campaign-ui"], produces = [MediaType.TEXT_PLAIN_VALUE])
+    @ResponseStatus(HttpStatus.OK)
+    fun handleInitialize(): String {
+        val viewName = "campaign-home"
+        return viewName
+    }
+}
