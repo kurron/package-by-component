0c79d6c0d0606f1526bd0231f6018142ff9316fe
rkurr <rkurr@jvmguy.com>
2024-06-27 21:07:13 +0000

    Basic model rending is working

--- asset-feature/src/main/kotlin/org/kurron/gurps/asset/AssetWebPage.kt
+++ asset-feature/src/main/kotlin/org/kurron/gurps/asset/AssetWebPage.kt
@@ -3,7 +3,9 @@ package org.kurron.gurps.asset
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -23,4 +25,25 @@ class AssetWebPage() {
         val viewName = "asset-home"
         return viewName
     }
+
+    @ModelAttribute
+    fun addArmor(model: Model) {
+        val data = listOf(Armor("Cloth Armor", 1, 150, 12),
+                         Armor("Leather Armor", 2, 340, 20))
+        model.addAttribute("ARMOR", data)
+/*
+        model.addAttribute("Cloth Armor", 1)
+        model.addAttribute("Leather Armor", 2)
+        model.addAttribute("Light Scale", 3)
+        model.addAttribute("Mail", 4)
+        model.addAttribute("Steel Laminate", 5)
+        model.addAttribute("Plate", 6)
+        model.addAttribute("Flak Jacket", 7)
+        model.addAttribute("Ballistic Vest", 8)
+        model.addAttribute("Tactical Vest", 12)
+*/
+    }
+
+    data class Armor(val type: String, val damageResistance: Int, val cost: Int, val weight: Int)
+
 }
