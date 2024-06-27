bb9c54737fa8cea1e5b5cea2bd4475d874a01630
rkurr <rkurr@jvmguy.com>
2024-06-27 15:48:05 +0000

    Provide a parent portion of the path

--- asset-feature/src/main/kotlin/org/kurron/gurps/asset/AssetWebPage.kt
+++ asset-feature/src/main/kotlin/org/kurron/gurps/asset/AssetWebPage.kt
@@ -4,10 +4,12 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @Controller
+@RequestMapping("/asset-ui")
 class AssetWebPage() {
 
     // POST /asset - create
@@ -15,7 +17,7 @@ class AssetWebPage() {
     // GET /asset/{id} - read
     // DELETE /asset/{id} - delete
     // bulk upload?
-    @GetMapping(path = ["/asset-ui"], produces = [MediaType.TEXT_PLAIN_VALUE])
+    @GetMapping(path = ["/home"], produces = [MediaType.TEXT_PLAIN_VALUE])
     @ResponseStatus(HttpStatus.OK)
     fun handleInitialize(): String {
         val viewName = "asset-home"
--- campaign-feature/src/main/kotlin/org/kurron/gurps/campaign/CampaignWebPage.kt
+++ campaign-feature/src/main/kotlin/org/kurron/gurps/campaign/CampaignWebPage.kt
@@ -4,9 +4,11 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @Controller
+@RequestMapping("/campaign-ui")
 class CampaignWebPage() {
 
     // POST /asset - create
@@ -14,7 +16,7 @@ class CampaignWebPage() {
     // GET /asset/{id} - read
     // DELETE /asset/{id} - delete
     // bulk upload?
-    @GetMapping(path = ["/campaign-ui"], produces = [MediaType.TEXT_PLAIN_VALUE])
+    @GetMapping(path = ["/home"], produces = [MediaType.TEXT_PLAIN_VALUE])
     @ResponseStatus(HttpStatus.OK)
     fun handleInitialize(): String {
         val viewName = "campaign-home"
