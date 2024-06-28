a11af030afd82c4d87d52e4af845a88a2765308d
rkurr <rkurr@jvmguy.com>
2024-06-28 21:10:24 +0000

    Display all armor attributes

--- asset-feature/src/main/kotlin/org/kurron/gurps/asset/AssetWebPage.kt
+++ asset-feature/src/main/kotlin/org/kurron/gurps/asset/AssetWebPage.kt
@@ -29,19 +29,16 @@ class AssetWebPage() {
     @ModelAttribute
     fun addArmor(model: Model) {
         val data = listOf(Armor("Cloth Armor", 1, 150, 12),
-                         Armor("Leather Armor", 2, 340, 20))
+                         Armor("Leather Armor", 2, 340, 20),
+                         Armor("Light Scale", 3, 610, 49),
+                         Armor("Mail", 4, 645, 58),
+                         Armor("Steel Laminate", 5, 1_360, 64),
+                         Armor("Plate", 6, 4_040, 90),
+                         Armor("Flak Jacket", 7, 500, 20),
+                         Armor("Ballistic Vest", 8, 400, 2),
+                         Armor("Tactical Vest", 12, 900, 9),
+        )
         model.addAttribute("ARMOR", data)
-/*
-        model.addAttribute("Cloth Armor", 1)
-        model.addAttribute("Leather Armor", 2)
-        model.addAttribute("Light Scale", 3)
-        model.addAttribute("Mail", 4)
-        model.addAttribute("Steel Laminate", 5)
-        model.addAttribute("Plate", 6)
-        model.addAttribute("Flak Jacket", 7)
-        model.addAttribute("Ballistic Vest", 8)
-        model.addAttribute("Tactical Vest", 12)
-*/
     }
 
     data class Armor(val type: String, val damageResistance: Int, val cost: Int, val weight: Int)
